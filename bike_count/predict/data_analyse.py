442e996868a0d0b5c23b5e92fa7f4aaf83032d1a
Assiab2707 <assiab2707@yahoo.com>
2021-03-31 19:53:41 +0200

    vis

--- bike_count/predict/data_analyse.py
+++ bike_count/predict/data_analyse.py
@@ -1,10 +1,10 @@
 import pandas as pd
-from statistics import median
+from statistics import mean, median, quantiles
 
 def summary_day(df_bikes):
     day = ['Lundi','Mardi','Mercredi', 'Jeudi', 'Vendredi','Samedi', 'Dimanche']
-    summary = pd.DataFrame(columns=day)
+    summary = pd.DataFrame(columns = ['day','mean','median', 'min', 'max','first_quantile', 'third_quantile' ])
     for i in range(len(day)): 
-        summary[day].append[day[i]]
-        summary.append(df_bikes[df_bikes.index.dayofweek==i].describe())
-        print (summary)
+        sub_df = df_bikes[df_bikes.index.dayofweek==i]['TotalDateSinceMidnight']
+        summary.loc[i] = [day[i], mean(sub_df), median(sub_df), min(sub_df), max(sub_df),quantiles(sub_df, n=4)[0], quantiles(sub_df, n=4)[2]]
+    return summary
--- script_predict_00_09.py
+++ script_predict_00_09.py
@@ -16,9 +16,30 @@ df_formated = bc.drop_columns_useless(df_formated,['Date','Time'])
 df_09 = bc.drop_duplicates_max(df_09)
 df_09 = bc.drop_columns_useless(df_09,['Date','Time'])
 
-bc.histogram_plot(df_09,"Représentation graphique des vélos entre 00h et 09h", "TotalDateSinceMidnight", 675)
+summary = bc.summary_day(df_09)
+# print(summary)
+
+
+# bc.histogram_plot(df_09,"Représentation graphique des vélos entre 00h et 09h", "TotalDateSinceMidnight", 675)
 
 df_09_without_we = bc.subdf_withoutweekend(df_09)
-bc.histogram_plot(df_09_without_we,"Représentation graphique des vélos du lundi au vendredi entre 00h et 09h", "TotalDateSinceMidnight", 675)
-bc.summary_day(df_09)
-print (df_09)
+# bc.histogram_plot(df_09_without_we,"Représentation graphique des vélos du lundi au vendredi entre 00h et 09h", "TotalDateSinceMidnight", 675)
+
+df_09_mon = bc.subdf_byweekday(df_09,0)
+df_09_tues = bc.subdf_byweekday(df_09,1)
+df_09_wed = bc.subdf_byweekday(df_09,2)
+df_09_thur = bc.subdf_byweekday(df_09,3)
+df_09_fri = bc.subdf_byweekday(df_09,4)
+df_09_sa = bc.subdf_byweekday(df_09,5)
+df_09_sun = bc.subdf_byweekday(df_09,6)
+# bc.sub_plot_days(df_09_mon, df_09_tues, df_09_wed, df_09_thur, df_09_fri, df_09_sa, df_09_sun,"TotalDateSinceMidnight",600)
+
+# print(df_09.head(50))
+date = pd.date_range(start='2020-03-16 09:00:00', end=str(pd.to_datetime('today').normalize()))
+for i in range(len(df_09)):
+    if (str(df_09.index[i])[0:10]!=str(date[i])[0:10]):
+        df_09.loc[date[i]] = summary['median'][date[i].dayofweek]
+        df_09 = df_09.sort_index(ascending=True)
+    
+
+print(df_09.head(30))
